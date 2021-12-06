a6e7edcc61ea95e3fda1f10ea776360abee9c8ad
kMachiraju3 <machiraju_kiranmai@columbusstate.edu>
2021-12-06 04:05:19 +0000

    656

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,7 +3,10 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
 
 @Controller
 public class HelloController {
@@ -25,8 +28,23 @@ public class HelloController {
 	}
 
 	@GetMapping("/xkcdSpec")
-	public String xkcdSpecific(@RequestParam(name="name", required=false, defaultValue="614") String id, Model model) {
+	public String xkcdSpecific(@RequestParam(name="name", required=false, defaultValue="200") String id, Model model) {
 		model.addAttribute("id", id);
 		return "xkcdSpecific";
+	}
+
+
+	Date Todaydate = new Date();  
+	SimpleDateFormat formatter = new SimpleDateFormat("yyyy/mm/dd");  
+	String currentdate= formatter.format(Todaydate); 
+
+
+	@GetMapping("/nasa")
+	public String nasaApod(@RequestParam(name="date", required=false) String date, Model model) {
+		if(date == null){
+			date = currentdate;
+		}
+		model.addAttribute("date", date);
+		return "nasa";
 	}
 }
